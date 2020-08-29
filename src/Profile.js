a70ba65e8f7eb2f571fb49a772efba14ab8277cc
hocnx <xuanhocbk@gmail.com>
2020-08-29 09:19:23 +0900

    fixbug error of post confirm signup no configured lambda triger

--- src/Profile.js
+++ src/Profile.js
@@ -1,72 +1,74 @@
 import React, { useState, useEffect } from 'react'
-import {AmplifyAuthenticator,  AmplifySignUp, AmplifySignOut, AmplifyConfirmSignUp} from '@aws-amplify/ui-react'
-import { Auth} from 'aws-amplify'
+import { AmplifySignUp} from '@aws-amplify/ui-react';
+import {AmplifyAuthenticator, AmplifySignOut, AmplifyConfirmSignUp} from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify'
 import { Avatar, Row, Col } from 'antd';
 import { UserOutlined} from '@ant-design/icons';
-import { Hub } from 'aws-amplify'
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { AuthState, onAuthUIStateChange} from '@aws-amplify/ui-components';
+
 
 import ProfilePictureUpload from './ProfilePictureUpload'
 import getProfileImage from './getProfileImage'
 
-function Profile() {
 
+const Profile = () => {
+    const [authState, setAuthState] = React.useState();
     const [user, setUser] = React.useState();
 
-    React.useEffect(() => {
+    useEffect(() => {
         return onAuthUIStateChange((nextAuthState, authData) => {
-            console.log('authData:::', authData)
+            console.log('authData:', authData)
+            console.log('nextAuthState:', nextAuthState)
+
+            setAuthState(nextAuthState);
             setUser(authData)
-            console.log('authData: ', authData)
         });
     }, []);
 
-        return user && user.attributes ?
-            (
-            <>
+  return authState === AuthState.SignedIn && user ? (
+    <>
+    <Row justify="center">
+        <Col span={8}>
+            <h1>Profile</h1>
+            <h2>Username: {user.username}</h2>
+            <h3>Email: {user.attributes.email}</h3>
+        </Col>
+        <Col span={4}>
             <Row justify="center">
                 <Col span={8}>
-                    <h1>Profile</h1>
-                    <h2>Username: {user.username}</h2>
-                    <h3>Email: {user.attributes.email}</h3>
-                </Col>
-                <Col span={4}>
-                    <Row justify="center">
-                        <Col span={8}>
-                            <Avatar size={128} src={getProfileImage(user.username)} icon={<UserOutlined />} />
-                        </Col>
-                    </Row>
-                    <Row justify="center">
-                        <Col span={24}>
-                            <ProfilePictureUpload username={user.username} reloadPage={()=>setUser({...user})}/>
-                        </Col>
-                    </Row>
+                    <Avatar size={128} src={getProfileImage(user.username)} icon={<UserOutlined />} />
                 </Col>
             </Row>
             <Row justify="center">
-                <Col span={4}>
-                    <AmplifySignOut />
+                <Col span={24}>
+                    <ProfilePictureUpload username={user.username} reloadPage={()=>setUser({...user})}/>
                 </Col>
             </Row>
-            </>
-        ) : (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        </Col>
+    </Row>
+    <Row justify="center">
+        <Col span={4}>
+            <AmplifySignOut />
+        </Col>
+    </Row>
+    </>
+    ) : (
+    <Row justify="center">
+    <Col span={8}>
         <AmplifyAuthenticator>
             <AmplifySignUp
             slot="sign-up"
             formFields={[
                 { type: "username" },
-                { type: "email" },
-                {
-                type: "password"
-                }
-            ]} 
+                { type: "password" },
+                { type: "email" }
+            ]}
             />
-            <AmplifyConfirmSignUp />
+            <AmplifyConfirmSignUp slot="confirm-sign-up"/>
         </AmplifyAuthenticator>
-        </div>
-    )
+    </Col>
+    </Row>
+      )
 }
 
-
-export default Profile
+export default Profile;
